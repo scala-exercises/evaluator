b5e7263c5a474256a47d97eac45958b0f7e56f86
Juan Pedro Moreno <juanpmorest@gmail.com>
2016-08-08 16:32:38 +0200

    Removes Id Monad Error based on CR comments

--- client/src/main/scala/org/scalaexercises/evaluator/implicits.scala
+++ client/src/main/scala/org/scalaexercises/evaluator/implicits.scala
@@ -7,13 +7,12 @@ package org.scalaexercises.evaluator
 
 import cats.std.FutureInstances
 import cats.std.future._
-import cats.{Eval, Id, Monad, MonadError}
+import cats.{Eval, MonadError}
 import org.scalaexercises.evaluator.free.interpreters.Interpreter
 
 object implicits
     extends Interpreter
     with EvalInstances
-    with IdInstances
     with FutureInstances
 
 trait EvalInstances {
@@ -40,33 +39,6 @@ trait EvalInstances {
             case e: Throwable ⇒ f(e).value
           }
         })
-    }
-
-}
-
-trait IdInstances {
-
-  implicit def idMonadError(implicit I: Monad[Id]): MonadError[Id, Throwable] =
-    new MonadError[Id, Throwable] {
-
-      override def pure[A](x: A): Id[A] = I.pure(x)
-
-      override def ap[A, B](ff: Id[A ⇒ B])(fa: Id[A]): Id[B] = I.ap(ff)(fa)
-
-      override def map[A, B](fa: Id[A])(f: Id[A ⇒ B]): Id[B] = I.map(fa)(f)
-
-      override def flatMap[A, B](fa: Id[A])(f: A ⇒ Id[B]): Id[B] =
-        I.flatMap(fa)(f)
-
-      override def raiseError[A](e: Throwable): Id[A] = throw e
-
-      override def handleErrorWith[A](fa: Id[A])(f: Throwable ⇒ Id[A]): Id[A] = {
-        try {
-          fa
-        } catch {
-          case e: Exception ⇒ f(e)
-        }
-      }
     }
 
 }
