2c6cf57103942448163f4aa06993984ebb0b3ed0
Juan Pedro Moreno <juanpmorest@gmail.com>
2020-06-09 16:55:44 +0200

    Bumps up dependencies and sbt

--- project/ProjectPlugin.scala
+++ project/ProjectPlugin.scala
@@ -17,19 +17,19 @@ object ProjectPlugin extends AutoPlugin {
   object autoImport {
 
     object V {
-      lazy val cats                = "2.1.0"
+      lazy val cats                = "2.1.1"
       lazy val catsEffect          = "2.1.3"
-      lazy val http4s              = "0.21.3"
+      lazy val http4s              = "0.21.4"
       lazy val circe               = "0.13.0"
       lazy val log4s               = "1.7.0"
       lazy val scalatest           = "3.1.2"
-      lazy val scalatestplusScheck = "3.1.1.1"
+      lazy val scalatestplusScheck = "3.1.2.0"
       lazy val jodaTime            = "2.10.6"
       lazy val slf4j               = "1.7.30"
       lazy val jwtCore             = "4.3.0"
-      lazy val coursier            = "2.0.0-RC6-12"
+      lazy val coursier            = "2.0.0-RC6-21"
       lazy val config              = "1.4.0"
-      lazy val scala               = "2.13.1"
+      lazy val scala               = "2.13.2"
     }
 
     lazy val dockerSettings = Seq(
