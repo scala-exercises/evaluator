5f4000f76298aa044a35b968961e53b81930976c
Juan Pedro Moreno <juanpmorest@gmail.com>
2020-06-29 09:14:00 +0200

    Updates build

--- project/ProjectPlugin.scala
+++ project/ProjectPlugin.scala
@@ -17,7 +17,7 @@ object ProjectPlugin extends AutoPlugin {
     object V {
       lazy val cats                = "2.1.1"
       lazy val catsEffect          = "2.1.3"
-      lazy val http4s              = "0.21.5"
+      lazy val http4s              = "0.21.6"
       lazy val circe               = "0.13.0"
       lazy val log4s               = "1.7.0"
       lazy val scalatest           = "3.2.0"
@@ -27,7 +27,7 @@ object ProjectPlugin extends AutoPlugin {
       lazy val jwtCore             = "4.3.0"
       lazy val coursier            = "2.0.0-RC6-21"
       lazy val config              = "1.4.0"
-      lazy val scala               = "2.13.2"
+      lazy val scala               = "2.13.3"
     }
 
     lazy val dockerSettings = Seq(
