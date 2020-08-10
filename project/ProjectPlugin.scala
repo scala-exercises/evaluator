88a1c056108c97c3dd98397d4abd00f507a45dea
47erbot <developer@47deg.com>
2020-08-10 07:14:40 +0200

    Update http4s-blaze-server, http4s-circe, ... to 0.21.7

--- project/ProjectPlugin.scala
+++ project/ProjectPlugin.scala
@@ -17,7 +17,7 @@ object ProjectPlugin extends AutoPlugin {
     object V {
       lazy val cats                = "2.1.1"
       lazy val catsEffect          = "2.1.4"
-      lazy val http4s              = "0.21.6"
+      lazy val http4s              = "0.21.7"
       lazy val circe               = "0.13.0"
       lazy val log4s               = "1.7.0"
       lazy val scalatest           = "3.2.1"
