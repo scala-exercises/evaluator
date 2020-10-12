f335be6524152283aff9f5b7213f9d55fb3b1426
47erbot <developer@47deg.com>
2020-10-12 05:20:51 +0000

    Update coursier, coursier-cache, ... to 2.0.2

--- project/ProjectPlugin.scala
+++ project/ProjectPlugin.scala
@@ -25,7 +25,7 @@ object ProjectPlugin extends AutoPlugin {
       lazy val jodaTime            = "2.10.6"
       lazy val slf4j               = "1.7.30"
       lazy val jwtCore             = "4.3.0"
-      lazy val coursier            = "2.0.1"
+      lazy val coursier            = "2.0.2"
       lazy val config              = "1.4.0"
       lazy val scala               = "2.13.3"
     }
