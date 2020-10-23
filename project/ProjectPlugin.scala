5c9b140b12605c55383b765eae8fa7b1af5630e8
47erbot <developer@47deg.com>
2020-10-23 07:28:03 +0200

    Update typesafe:config to 1.4.1

--- project/ProjectPlugin.scala
+++ project/ProjectPlugin.scala
@@ -26,7 +26,7 @@ object ProjectPlugin extends AutoPlugin {
       lazy val slf4j               = "1.7.30"
       lazy val jwtCore             = "4.3.0"
       lazy val coursier            = "2.0.3"
-      lazy val config              = "1.4.0"
+      lazy val config              = "1.4.1"
       lazy val scala               = "2.13.3"
     }
 
