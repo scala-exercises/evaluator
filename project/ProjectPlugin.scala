052aaee8c0af0a4fad46cb62636c5052bc73e7c1
47erbot <developer@47deg.com>
2022-02-18 05:31:29 +0000

    Update jwt-core to 9.0.4

--- project/ProjectPlugin.scala
+++ project/ProjectPlugin.scala
@@ -24,7 +24,7 @@ object ProjectPlugin extends AutoPlugin {
       lazy val scalatestplusScheck = "3.2.2.0"
       lazy val jodaTime            = "2.10.13"
       lazy val slf4j               = "1.7.36"
-      lazy val jwtCore             = "9.0.3"
+      lazy val jwtCore             = "9.0.4"
       lazy val coursier            = "2.0.16"
       lazy val config              = "1.4.2"
       lazy val scala               = "2.13.8"
