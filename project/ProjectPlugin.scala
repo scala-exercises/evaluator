ba8c6146f726219732d2d04b54eab81c74d1731c
47erbot <developer@47deg.com>
2022-02-28 05:33:02 +0000

    Update cats-effect to 3.3.6

--- project/ProjectPlugin.scala
+++ project/ProjectPlugin.scala
@@ -16,7 +16,7 @@ object ProjectPlugin extends AutoPlugin {
 
     object V {
       lazy val cats                = "2.7.0"
-      lazy val catsEffect          = "2.5.4"
+      lazy val catsEffect          = "3.3.6"
       lazy val http4s              = "0.21.32"
       lazy val circe               = "0.14.1"
       lazy val log4s               = "1.7.0"
