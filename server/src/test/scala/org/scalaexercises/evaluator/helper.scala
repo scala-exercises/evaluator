f7180c877b7af07038399af1bf0974d71b3b220d
fortyseven_degrees <developer@47deg.com>
2022-10-14 06:14:36 +0000

    Update scala-compiler, scala-library, ... to 2.13.10

--- server/src/test/scala/org/scalaexercises/evaluator/helper.scala
+++ server/src/test/scala/org/scalaexercises/evaluator/helper.scala
@@ -23,7 +23,7 @@ object helper {
 
   sealed abstract class ScalaVersion(val version: String)
 
-  case object Scala213 extends ScalaVersion("2.13.9")
+  case object Scala213 extends ScalaVersion("2.13.10")
 
   def toScalaVersion(v: String): ScalaVersion =
     v match {
