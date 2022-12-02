9a0b79ab6c871fc0de7040da8049e30040e9b60b
fortyseven_degrees <developer@47deg.com>
2022-12-02 05:18:34 +0000

    Update joda-time to 2.12.2

--- project/ProjectPlugin.scala
+++ project/ProjectPlugin.scala
@@ -22,7 +22,7 @@ object ProjectPlugin extends AutoPlugin {
       lazy val log4s               = "1.7.0"
       lazy val scalatest           = "3.2.14"
       lazy val scalatestplusScheck = "3.2.2.0"
-      lazy val jodaTime            = "2.12.1"
+      lazy val jodaTime            = "2.12.2"
       lazy val slf4j               = "2.0.5"
       lazy val jwtCore             = "9.1.2"
       lazy val coursier            = "2.0.16"
